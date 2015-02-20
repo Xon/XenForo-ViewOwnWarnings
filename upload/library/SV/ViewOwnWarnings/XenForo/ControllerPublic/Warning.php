7e20973d2a1e2a33aab9be5f50d7d0722aab1f31
Xon <tazipper1@hotmail.com>
2015-02-20 22:53:55 +0800

    Tweaks & misc cleanup.

--- upload/library/SV/ViewOwnWarnings/XenForo/ControllerPublic/Warning.php
+++ upload/library/SV/ViewOwnWarnings/XenForo/ControllerPublic/Warning.php
@@ -12,7 +12,6 @@ class SV_ViewOwnWarnings_XenForo_ControllerPublic_Warning extends XFCP_SV_ViewOw
         $warningId = $this->_input->filterSingle('warning_id', XenForo_Input::UINT);
         $warning = $this->_getWarningOrError($warningId);
 
-        $canViewWarningDetails = $this->_getUserModel()->canViewWarnings();
         $visitor = XenForo_Visitor::getInstance();
         if ($warning['user_id'] == $visitor['user_id'])
         {
